5925de2528ddcec87f23d8de9808dc69a19ca6a7
Jacob Kelly <jskelly2021@gmail.com>
2025-06-22 22:25:50 -0500

    HW6: All NETS functional

--- HW6/HW6.py
+++ HW6/HW6.py
@@ -17,6 +17,12 @@ def extract(df: pd.DataFrame, class1, class2, feat1, feat2) -> Tuple[pd.DataFram
     labels = filtered_df['variety'].map({class1: 0, class2: 1})
     return features, labels
 
+def extract_all_feat(df: pd.DataFrame, class1, class2) -> Tuple[pd.DataFrame, pd.Series]:
+    filtered_df = df[df['variety'].isin([class1, class2])]
+    features = filtered_df.drop('variety', axis=1)
+    labels = filtered_df['variety'].map({class1: 0, class2: 1})
+    return features, labels
+
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
@@ -48,7 +54,7 @@ class NN:
             nets, activations = self.forwardPass(X)
 
             # backpropogation
-            self.backpropagate(X, t, nets, activations)
+            self.backpropagate(t, nets, activations)
 
             # find the cost function
             if epoch % 10 == 0:
@@ -58,6 +64,7 @@ class NN:
         return costs
 
     def forwardPass(self, X):
+        X = np.array(X)
         nets = []
         activations = [X]
 
@@ -70,7 +77,8 @@ class NN:
 
         return nets, activations
 
-    def backpropagate(self, X, t, nets, activations):
+    def backpropagate(self, t, nets, activations):
+        t = np.array(t)
         deltas = [None] * len(self.W)
         deltas[-1] = (activations[-1] - t) * function_derivative(nets[-1])
 
@@ -95,7 +103,7 @@ class NN:
 
 
 def NET(set, test, hidden_layers, title=""):
-    nn = NN(features=2, hidden_layers=hidden_layers, output_neurons=1, learning_rate=0.01)
+    nn = NN(features=set[0].shape[1], hidden_layers=hidden_layers, output_neurons=1, learning_rate=0.01)
     cost = nn.train(set[0], set[1])
 
     y_pred = nn.predict(test[0])
@@ -148,22 +156,38 @@ if __name__ == "__main__":
     set1_test = (X1_test, t1_test)
     set2_test = (X2_test, t2_test)
 
-    # visualizeData(X1, t1, "Versicolor vs Virginica (sepal features)", ("Versicolor", "Virginica"))
-    # visualizeData(X2, t2, "Setosa vs Virginica (petal features)", ("Setosa", "Virginica"))
+    X3, t3 = extract_all_feat(train_df, 'Versicolor', 'Virginica')
+    X3_test, t3_test = extract_all_feat(test_df, 'Versicolor', 'Virginica')
+
+    X4, t4 = extract_all_feat(train_df, 'Setosa', 'Virginica')
+    X4_test, t4_test = extract_all_feat(test_df, 'Setosa', 'Virginica')
+
+    t3 = t3.values.reshape([-1, 1])
+    t4 = t4.values.reshape([-1, 1])
+    t3_test = t3_test.values.reshape([-1, 1])
+    t4_test = t4_test.values.reshape([-1, 1])
+
+    set3 = (X3, t3)
+    set4 = (X4, t4)
+    set3_test = (X3_test, t3_test)
+    set4_test = (X4_test, t4_test)
+
+    visualizeData(X1, t1, "Versicolor vs Virginica (sepal features)", ("Versicolor", "Virginica"))
+    visualizeData(X2, t2, "Setosa vs Virginica (petal features)", ("Setosa", "Virginica"))
 
     # NET 1
-    # NET(set1, set1_test, hidden_layers=[5], title="NET1: Versicolor vs Virginica (sepal features)")
-    # NET(set2, set2_test, hidden_layers=[5], title="NET1: Setosa vs Virginica (petal features)")
+    NET(set1, set1_test, hidden_layers=[5], title="NET1: Versicolor vs Virginica (sepal features)")
+    NET(set2, set2_test, hidden_layers=[5], title="NET1: Setosa vs Virginica (petal features)")
 
     # NET 2
-    # NET(set1, set1_test, hidden_layers=[20], title="NET2: Versicolor vs Virginica (sepal features)")
-    # NET(set2, set2_test, hidden_layers=[20], title="NET2: Setosa vs Virginica (petal features)")
+    NET(set1, set1_test, hidden_layers=[20], title="NET2: Versicolor vs Virginica (sepal features)")
+    NET(set2, set2_test, hidden_layers=[20], title="NET2: Setosa vs Virginica (petal features)")
 
     # NET 3
-    # NET(set1, set1_test, hidden_layers=[10, 5], title="NET3: Versicolor vs Virginica (sepal features)")
-    # NET(set2, set2_test, hidden_layers=[10, 5], title="NET3: Setosa vs Virginica (petal features)")
+    NET(set1, set1_test, hidden_layers=[10, 5], title="NET3: Versicolor vs Virginica (sepal features)")
+    NET(set2, set2_test, hidden_layers=[10, 5], title="NET3: Setosa vs Virginica (petal features)")
 
     # NET 4
-    NET(set1, set1_test, hidden_layers=[5], title="NET4: Versicolor vs Virginica (sepal features)")
-    NET(set2, set2_test, hidden_layers=[5], title="NET4: Setosa vs Virginica (petal features)")
+    NET(set3, set3_test, hidden_layers=[5], title="NET4: Versicolor vs Virginica (all features)")
+    NET(set4, set4_test, hidden_layers=[5], title="NET4: Setosa vs Virginica (all features)")
 
