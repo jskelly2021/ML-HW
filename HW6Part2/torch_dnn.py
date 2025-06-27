6381344036199d1d8bd566b60339342bd4bf7693
Jacob Kelly <jskelly2021@gmail.com>
2025-06-27 17:06:32 -0500

    HW6Part2: All torch funcs implemented

--- HW6Part2/torch_dnn.py
+++ HW6Part2/torch_dnn.py
@@ -84,7 +84,7 @@ def make_classification_model(
         layers.append(torch.nn.Linear(n_neurons, n_neurons))
         layers.append(get_activation(activation))
 
-    layers.append(torch.nn.Softmax(n_neurons, n_classes))
+    layers.append(torch.nn.Linear(n_neurons, n_classes))
 
     return torch.nn.Sequential(*layers)
 
@@ -109,7 +109,28 @@ def train_epoch(
     Returns:
         tuple: (epoch_loss, epoch_accuracy) containing the average loss and accuracy for the epoch
     """
-    return float("inf"), 0
+    model.train()
+    epoch_loss, correct = 0, 0 
+
+    for X, y in train_loader:
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        epoch_loss += loss.item() * len(X)
+        if classification:
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    epoch_loss = epoch_loss / len(train_loader.dataset)
+    if classification:
+        epoch_accuracy = correct / len(train_loader.dataset)
+    else:
+        epoch_accuracy = epoch_loss
+
+    return epoch_loss, epoch_accuracy
 
 
 def evaluate_epoch(
@@ -133,7 +154,29 @@ def evaluate_epoch(
             - val_accuracy (float): Average accuracy on test data (for classification)
             - predictions (np.ndarray): Model predictions on test data
     """
-    return float("inf"), 0, np.array([])
+    model.eval()
+    val_loss, correct = 0, 0
+    preds = []
+
+    with torch.no_grad():
+        for X, y in test_loader:
+            pred = model(X)
+            val_loss += loss_fn(pred, y).item() * len(X)
+
+            if classification:
+                correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+            preds.append(pred)
+
+    val_loss = val_loss / len(test_loader.dataset)
+    if classification:
+        val_accuracy = correct / len(test_loader.dataset)
+    else:
+        val_accuracy = val_loss
+
+    predictions = torch.cat(preds, dim=0)
+
+    return val_loss, val_accuracy, np.array(predictions)
 
 
 def is_classification(model: torch.nn.Sequential) -> bool:
@@ -146,7 +189,8 @@ def is_classification(model: torch.nn.Sequential) -> bool:
     Returns:
         bool: True if the model is for classification, False otherwise
     """
-    return False
+    return model[-1] == torch.nn.Softmax
+
 
 
 def get_dataloaders(
@@ -167,14 +211,29 @@ def get_dataloaders(
     Returns:
         tuple: (train_loader, test_loader) containing DataLoader objects for training and test data
     """
-
-    train_dataset = torch.utils.data.TensorDataset(torch.from_numpy(x_train), torch.from_numpy(y_train))
-    test_dataset = torch.utils.data.TensorDataset(torch.from_numpy(x_test), torch.from_numpy(y_test))
+    train_dataset = torch.utils.data.TensorDataset(torch.from_numpy(x_train).float(), torch.from_numpy(y_train).float())
+    test_dataset = torch.utils.data.TensorDataset(torch.from_numpy(x_test).float(), torch.from_numpy(y_test).float())
 
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE)
 
     return train_loader, test_loader
+
+
+def get_model_params(model, is_class, opt, lr):
+    if opt == 'SGD':
+        optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    elif opt == 'Adam':
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    else:
+        optimizer = torch.optim.RMSprop(model.parameters(), lr=lr)
+
+    if is_class:
+        loss = torch.nn.CrossEntropyLoss()
+    else:
+        loss = torch.nn.MSELoss()
+
+    return optimizer, loss
 
 
 def train_and_evaluate(
@@ -185,7 +244,7 @@ def train_and_evaluate(
     model: torch.nn.Sequential,
     optimizer: str,
     learning_rate: float = 0.01,
-) -> [np.ndarray, float, float, dict]:
+) -> tuple[np.ndarray, float, float, dict]:
     """
     Train and evaluate a PyTorch model.
 
@@ -205,5 +264,22 @@ def train_and_evaluate(
             - float: Test metric (same as test loss for regression, accuracy for classification)
             - dict: Training history containing loss and metrics
     """
+    train_loader, test_loader = get_dataloaders(x_train, x_test, y_train, y_test)
+
+    is_class = is_classification(model)
+    model_optimizer, model_loss = get_model_params(model, is_class, optimizer, learning_rate)
+
     history = {"loss": [], "val_loss": [], "accuracy": [], "val_accuracy": []}
-    return np.array([]), 0, 0, history
+
+    for _ in range(EPOCHS):
+        train_loss, train_accuracy = train_epoch(model, model_optimizer, model_loss, train_loader, is_class)
+        test_loss, test_accuracy, _ = evaluate_epoch(model, model_loss, test_loader, is_class)
+
+        history["loss"].append(train_loss)
+        history["val_loss"].append(test_loss)
+        history["accuracy"].append(train_accuracy)
+        history["val_accuracy"].append(test_accuracy)
+
+    test_loss, test_accuracy, preds = evaluate_epoch(model, model_loss, test_loader, is_class)
+
+    return np.array(preds), test_loss, test_accuracy, history
